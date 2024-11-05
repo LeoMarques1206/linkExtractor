0caa1ea84e8f65ed04206dd5caee3f472621ff6a
Leonardo Marques <leogmarq@gmail.com>
2024-11-05 18:46:47 -0300

    commit

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,50 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, TaskSet, task, between
 
-class LinkExtractorUser(HttpUser):
-    wait_time = between(1, 2)  # Tempo de espera entre as requisições 
-
-    @task
+class LinkExtractorRubyBehavior(TaskSet):
+    @task(10)
     def extract_links(self):
-        url = "http://localhost:5000/extract?url=http://example.com"  # Endereço do serviço Link Extractor
-        self.client.get(url)
+        urls = [
+            "http://localhost:4567/extract?url=https://www.google.com",
+            "http://localhost:4567/extract?url=https://www.wikipedia.org",
+            "http://localhost:4567/extract?url=https://www.github.com",
+            "http://localhost:4567/extract?url=https://www.stackoverflow.com",
+            "http://localhost:4567/extract?url=https://www.medium.com",
+            "http://localhost:4567/extract?url=https://www.reddit.com",
+            "http://localhost:4567/extract?url=https://www.bbc.com",
+            "http://localhost:4567/extract?url=https://www.cnn.com",
+            "http://localhost:4567/extract?url=https://www.nytimes.com",
+            "http://localhost:4567/extract?url=https://www.linkedin.com"
+        ]
+        for url in urls:
+            self.client.get(url)
+
+
+class LinkExtractorPythonBehavior(TaskSet):
+    @task(10)
+    def extract_links(self):
+        urls = [
+            "http://localhost:5000/extract?url=https://www.google.com",
+            "http://localhost:5000/extract?url=https://www.wikipedia.org",
+            "http://localhost:5000/extract?url=https://www.github.com",
+            "http://localhost:5000/extract?url=https://www.stackoverflow.com",
+            "http://localhost:5000/extract?url=https://www.medium.com",
+            "http://localhost:5000/extract?url=https://www.reddit.com",
+            "http://localhost:5000/extract?url=https://www.bbc.com",
+            "http://localhost:5000/extract?url=https://www.cnn.com",
+            "http://localhost:5000/extract?url=https://www.nytimes.com",
+            "http://localhost:5000/extract?url=https://www.linkedin.com"
+        ]
+        for url in urls:
+            self.client.get(url)
+
+
+class LinkExtractorRubyUser(HttpUser):
+    tasks = [LinkExtractorRubyBehavior]
+    wait_time = between(1, 2)
+    host = "http://localhost:4567"
+
+
+class LinkExtractorPythonUser(HttpUser):
+    tasks = [LinkExtractorPythonBehavior]
+    wait_time = between(1, 2)
+    host = "http://localhost:5000"
